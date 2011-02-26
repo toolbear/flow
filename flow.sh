109ebb8fc6b48662cb0f89b005cfcb2cb6b0fcda
Tim Taylor <tim@tool-man.org>
2011-02-26 00:25:26 -0500

    rework flow scripts to be compiled to a single app

--- flow.sh
+++ flow.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash
+# Flow
+# Diminish distracting applications to achieve flow, restoring them when done.
 
 if [ ! -d "$FLOW_DIR" ]; then
     export FLOW_DIR=$(cd $(dirname ${BASH_SOURCE[0]:-$0}); pwd)
 fi
 
 flow() {
+    trap 'local error=$?; trap - ERR; return $error' ERR
+    trap 'trap - RETURN; set +E' RETURN
+    set -E
+
     SCRIPTDIR=$FLOW_DIR/flow.d
     if [ -d $SCRIPTDIR ] ; then
         local action
@@ -26,7 +31,7 @@ function usage() {
 usage: flow COMMAND
 
 Commands:
-  install  Install `Enter Flow` and `Leave Flow` to $HOME/Applications
+  install    Install `Enter Flow.app` and `Leave Flow.app` to $HOME/Applications
 EOF
 #
 #  enter    Enter a state of flow by reducing distractions
@@ -35,8 +40,8 @@ EOF
 }
 
 function install() {
-    local APPLICATIONS='$HOME/Applications'
-    local TARGET='$FLOW_DIR/target'
+    local APPLICATIONS=$HOME/Applications
+    local TARGET=$FLOW_DIR/target
 
     mkdir -p $TARGET
     local SCRIPTS=`flow_scripts`
@@ -44,10 +49,10 @@ function install() {
     cat $FLOW_DIR/include/restore.scpt $FLOW_DIR/include/common.scpt $SCRIPTS |osacompile -o $TARGET/Leave\ Flow.app
 
     mkdir -p $APPLICATIONS
-    ln -sf $TARGET/Enter\ Flow.app $APPLICATIONS/Enter\ Flow.app
-    ln -sf $TARGET/Leave\ Flow.app $APPLICATIONS/Leave\ Flow.app
+    rm -rf $APPLICATIONS/Enter\ Flow.app && ln -sh $TARGET/Enter\ Flow.app $APPLICATIONS
+    rm -rf $APPLICATIONS/Leave\ Flow.app && ln -sh $TARGET/Leave\ Flow.app $APPLICATIONS
 
-    printf "Installed \`Enter Flow\` and \`Leave Flow\` to $APPLICATIONS\n"
+    printf "Installed \`Enter Flow.app\` and \`Leave Flow.app\`\n"
 }
 
 function flow_scripts() {
---
+++ include/diminish.scpt
@@ -0,0 +1,1 @@
+property flowAction : "diminish"
