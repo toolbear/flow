b6ab83048763b8354526210862c6e7975699357d
Tim Taylor <tim@tool-man.org>
2011-05-12 04:16:41 -0400

    inlined most functions to avoid shell pollution

--- flow.sh
+++ flow.sh
@@ -1,60 +1,56 @@
 # Flow
 # Diminish distracting applications to achieve flow, restoring them when done.
 
-if [ ! -d "$FLOW_DIR" ]; then
+if [[ ! -d "$FLOW_DIR" ]]; then
     export FLOW_DIR=$(cd $(dirname ${BASH_SOURCE[0]:-$0}); pwd)
 fi
 
 flow() {
-    trap 'local error=$?; trap - ERR; return $error' ERR
-    trap 'trap - RETURN; set +E' RETURN
-    set -E
-
-    SCRIPTDIR=$FLOW_DIR/flow.d
-    if [ -d $SCRIPTDIR ] ; then
-        local action
-        case $1 in
-            install)
-                install
-                ;;
-            *)
-                usage
-                ;;
-        esac
-
-        printf "\n"
-    fi
-}
-
-function usage() {
-    cat <<'EOF'
-usage: flow COMMAND
-
-Commands:
-  install    Install `Enter Flow.app` and `Leave Flow.app` to $HOME/Applications
-EOF
-#
-#  enter    Enter a state of flow by reducing distractions
-#  leave    Leave a state of flow, restore communications, notifications, etc.
-#EOF
-}
-
-function install() {
-    local APPLICATIONS=$HOME/Applications
     local TARGET=$FLOW_DIR/target
+    local SCRIPTDIR=$FLOW_DIR/flow.d
+    
+    case $1 in
+        compile)
+            [[ -d "$TARGET" ]] && return # FIXME: recompile when scripts modifed
 
-    mkdir -p $TARGET
-    local SCRIPTS=`flow_scripts`
-    cat $FLOW_DIR/include/diminish.scpt $FLOW_DIR/include/common.scpt $SCRIPTS |osacompile -o $TARGET/Enter\ Flow.app
-    cat $FLOW_DIR/include/restore.scpt $FLOW_DIR/include/common.scpt $SCRIPTS |osacompile -o $TARGET/Leave\ Flow.app
+            mkdir -p $TARGET
+            local SCRIPTS=`find -L $SCRIPTDIR \( -type f -o -type l \) -regex '.*/[0-9][0-9][0-9][^/]*'`
+            cat $FLOW_DIR/include/diminish.scpt $FLOW_DIR/include/common.scpt $SCRIPTS |osacompile -o $TARGET/Enter\ Flow.app
+            cat $FLOW_DIR/include/restore.scpt $FLOW_DIR/include/common.scpt $SCRIPTS |osacompile -o $TARGET/Leave\ Flow.app
+            ;;
 
-    mkdir -p $APPLICATIONS
-    rm -rf $APPLICATIONS/Enter\ Flow.app && ln -sh $TARGET/Enter\ Flow.app $APPLICATIONS
-    rm -rf $APPLICATIONS/Leave\ Flow.app && ln -sh $TARGET/Leave\ Flow.app $APPLICATIONS
+        enter)
+            flow compile
+            $TARGET/Enter\ Flow.app/Contents/MacOS/applet
+            ;;
 
-    printf "Installed \`Enter Flow.app\` and \`Leave Flow.app\`\n"
-}
+        leave)
+            flow compile
+            $TARGET/Leave\ Flow.app/Contents/MacOS/applet
+            ;;
 
-function flow_scripts() {
-    find -L $SCRIPTDIR \( -type f -o -type l \) -regex '.*/[0-9][0-9][0-9][^/]*'
+        install)
+            flow compile
+            local APPLICATIONS=$HOME/Applications
+
+            mkdir -p $APPLICATIONS
+            rm -rf $APPLICATIONS/Enter\ Flow.app && ln -sh $TARGET/Enter\ Flow.app $APPLICATIONS
+            rm -rf $APPLICATIONS/Leave\ Flow.app && ln -sh $TARGET/Leave\ Flow.app $APPLICATIONS
+
+            printf "Installed \`Enter Flow.app\` and \`Leave Flow.app\`\n"
+            ;;
+
+        *)
+            cat <<'EOF' |cut -c17-
+                usage: flow COMMAND
+                   
+                Commands:
+                  enter      Enter a state of flow by reducing distractions
+                  leave      Leave a state of flow, restore communications, notifications, etc.
+
+                  install    Install `Enter Flow.app` and `Leave Flow.app` to $HOME/Applications
+
+EOF
+            ;;
+    esac
 }
