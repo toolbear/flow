37393d4567662c73d090d36329fdd34573309e77
Tim Taylor <tim@tool-man.org>
2011-05-12 20:12:08 -0400

    commands to manage/unmanage distracting apps

--- flow.sh
+++ flow.sh
@@ -6,15 +6,19 @@ if [[ ! -d "$FLOW_DIR" ]]; then
 fi
 
 flow() {
-    local TARGET=$FLOW_DIR/target
-    local SCRIPTDIR=$FLOW_DIR/flow.d
+    local TARGET="$FLOW_DIR/target"
+    local SCRIPTDIR="$FLOW_DIR/flow.d"
+    local MANAGED="$FLOW_DIR/managed"
     
     case $1 in
         compile)
             [[ -d "$TARGET" ]] && return # FIXME: recompile when scripts modifed
+            flow force-compile
+            ;;
 
+        force-compile)
             mkdir -p $TARGET
-            local SCRIPTS=`find -L $SCRIPTDIR \( -type f -o -type l \) -regex '.*/[0-9][0-9][0-9][^/]*'`
+            local SCRIPTS=$(find -L "$MANAGED" \( -type f -o -type l \) -regex '.*/[0-9][0-9][0-9][^/]*')
             cat $FLOW_DIR/include/diminish.scpt $FLOW_DIR/include/common.scpt $SCRIPTS |osacompile -o $TARGET/Enter\ Flow.app
             cat $FLOW_DIR/include/restore.scpt $FLOW_DIR/include/common.scpt $SCRIPTS |osacompile -o $TARGET/Leave\ Flow.app
             ;;
@@ -27,6 +31,24 @@ flow() {
         leave)
             flow compile
             $TARGET/Leave\ Flow.app/Contents/MacOS/applet
+            ;;
+
+        manage)
+            if [[ ! -d  "$MANAGED" ]] ; then
+                mkdir -p "$MANAGED"
+            fi
+            local APP="$2"
+            ln -shf "$SCRIPTDIR"/*_"$APP" "$MANAGED"
+            flow force-compile
+            ;;
+
+        unmanage)
+            [[ -d "$MANAGED" ]] || return
+
+            local APP="$2"
+            if [[ "$(find $MANAGED -type l -regex .*/[0-9][0-9][0-9]_$APP -exec rm -v {} ';')" ]] ; then
+                flow force-compile
+            fi
             ;;
 
         install)
@@ -42,13 +64,15 @@ flow() {
 
         *)
             cat <<'EOF' |cut -c17-
-                usage: flow COMMAND
+                usage: flow COMMAND ...
                    
                 Commands:
-                  enter      Enter a state of flow by reducing distractions
-                  leave      Leave a state of flow, restore communications, notifications, etc.
+                  enter          Enter a state of flow by reducing distractions
+                  leave          Leave a state of flow, restore communications, notifications, etc.
+                  manage APP     Add APP to list of distracting apps; diminish it when entering flow
+                  unmanage APP   Remove APP from list of distracting apps
 
-                  install    Install `Enter Flow.app` and `Leave Flow.app` to $HOME/Applications
+                  install        Install `Enter Flow.app` and `Leave Flow.app` to $HOME/Applications
 
 EOF
             ;;
